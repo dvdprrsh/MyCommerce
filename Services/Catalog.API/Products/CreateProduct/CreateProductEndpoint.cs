c119e01d926d4b47ceb280690fde17cb6efbd440
David Parrish <david@deopea.dev>
2026-04-11 15:31:34 +0100

    feat: add product model and base create product command

---
+++ Services/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,3 @@
+namespace Catalog.API.Products.CreateProduct;
+
+public class CreateProductEndpoint { }
---
+++ Services/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Categories,
+    string Description,
+    string Image,
+    decimal Price
+) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler
+    : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(
+        CreateProductCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
